71880024b9c46b38f7e51521d61e00a39f913f9a
YJSoft <yjsoft@yjsoft.pe.kr>
2017-11-15 07:47:09 +0900

    Complete skin config

--- customalert.admin.controller.php
+++ customalert.admin.controller.php
@@ -7,10 +7,23 @@ class customalertAdminController extends customalert
 
 	function procCustomalertAdminInsertConfig()
 	{
-		$vars = Context::getRequestVars();
+		$oModuleController = getController('module');
+		$oModuleModel = getModel('module');
+
+		$args = Context::getRequestVars();
+		$args->module = 'customalert';
+		$args->mid = 'customalert_module_2fa';
+		$info = $oModuleModel->getModuleInfoByMid('customalert_module_2fa');
+		$args->module_srl = $info->module_srl;
+		if(!$args->skin) $args->skin='default';
 
 		$oModuleController = getController('module');
-		$oModuleController->updateModuleConfig('customalert', $vars);
+		$oModuleController->updateModuleConfig('customalert', $args);
+
+		$output = $oModuleController->updateModule($args);
+		if(!$output->toBool()) {
+			return $output;
+		}
 		if(!in_array(Context::getRequestMethod(),array('XMLRPC','JSON')))
 		{
 			$returnUrl = Context::get('success_return_url') ? Context::get('success_return_url') : getNotEncodedUrl('', 'module', 'admin', 'act', 'dispCustomalertAdminConfig');
