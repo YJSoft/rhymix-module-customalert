6fcfca262bc723b4046139220c404f059578e77f
YJSoft <yjsoft@yjsoft.pe.kr>
2017-11-15 07:31:07 +0900

    Add update method

--- customalert.class.php
+++ customalert.class.php
@@ -58,6 +58,14 @@ class customalert extends ModuleObject
 			return true;
 		}
 
+		if($module_info->module_srl)
+		{
+			if($module_info->module != 'customalert')
+			{
+				return true;
+			}
+		}
+
 		return false;
 	}
 
@@ -71,6 +79,30 @@ class customalert extends ModuleObject
 			if(!$oModuleModel->getTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]))
 			{
 				$oModuleController->insertTrigger($trigger[0], $trigger[1], $trigger[2], $trigger[3], $trigger[4]);
+			}
+		}
+
+		$module_info = $oModuleModel->getModuleInfoByMid('customalert_module_2fa');
+		if($module_info->module_srl)
+		{
+			if($module_info->module != 'customalert')
+			{
+				return new Object(1,'already_installed_or_mid_duplicate');
+			}
+		}
+		else
+		{
+			$args = new stdClass;
+			$args->mid = 'customalert_module_2fa';
+			$args->module = 'customalert';
+			$args->browser_title = "DO_NOT_CHANGE_OR_USE_THIS_MID";
+			$args->site_srl = 0;
+			$args->skin = 'default';
+			$args->order_type = 'desc';
+			$output = $oModuleController->insertModule($args);
+			if(!$output->toBool())
+			{
+				return new Object(-1, 'msg_invalid_request');
 			}
 		}
 
