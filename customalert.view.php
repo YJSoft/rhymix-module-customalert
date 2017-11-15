c7fa88b003e7f035514702bfefe8d105353215ff
YJSoft <yjsoft@yjsoft.pe.kr>
2017-11-15 15:44:21 +0900

    debug

--- customalert.view.php
+++ customalert.view.php
@@ -18,8 +18,8 @@ class customalertView extends customalert
 	function dispCustomalert2fa() {
 		$oModuleModel = getModel('module');
 		$module_info = $oModuleModel->getModuleInfoByModuleSrl('customalert_module_2fa'); //
-		$module_info = $oModuleModel->addModuleExtraVars($module_info);
-		Context::set('module_info', $module_info);
+		$extra_vars = $oModuleModel->getModuleExtraVars($module_info->module_srl);
+		Context::set('module_info', $extra_vars);
 
 		$this->setTemplateFile('alert');
 	}
